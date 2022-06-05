c85e8175e4808d7fe099ffb00ba3d26fb7d4d6e9
collinsnavak <collinsnavak@gmail.com>
2022-06-05 13:41:12 -0700

    Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,36 +1,101 @@
-#include <stdio.h>
-
 #include <stdlib.h>
 
 #include <time.h>
 
-/**
- * main - Entry point
- * this prints positive or negative
- * number
+
+
+/** 
  *
- * Return: Always 0 (success)
- */
+ *  * main - Evaluating random numbers
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
 
-int main(void)
-{
-int n;
-strand(time(0));
-n = rand() - RAND _ MAX / 2;
 
-if (n > 0)
-{
-printf("%d is a positive\n", n);
-}
-else if (n < 0) 
-{
-printf("%d is a negative\n", n);
-}
-else if (n == 0)
-{
-printf("%d is zero\n", n);
-}
 
-return (0);
+int main(void){
+
+	    int n;
+
+	        srand(time(0));
+
+
+
+		    n = rand() - RAND_MAX / 2;
+
+
+
+		        if (n > 0)
+
+				        printf("%d is positive\n", n);
+
+
+
+			    else if (n == 0)
+
+				            printf("%d is zero\n", n);
+
+
+
+			        else
+
+					        printf("%d is negative\n", n);
+
+
+
+				   return(0);
+
+				   /**
+				    *
+				    * * main - Evaluating random numbers
+				    *
+				    * *
+				    *
+				    * * Return: Always 0 (Success)
+				    *
+				    * */
+
+
+
+				   int main(void)
+
+				   {
+
+
+
+					   	int n;
+
+
+
+							srand(time(0));
+
+								n = rand() - RAND_MAX / 2;
+
+
+
+									if (n > 0)
+
+												printf("%d is positive\n", n);
+
+
+
+										else if (n == 0)
+
+													printf("%d is zero\n", n);
+
+
+
+											else
+
+														printf("%d is negative\n", n);
+
+
+
+												return (0);
+
+				   }
 
 
