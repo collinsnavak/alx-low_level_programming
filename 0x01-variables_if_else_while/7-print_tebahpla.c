3d7429638a1786e4ae040f09eda58d300e711ab6
collinsnavak <collinsnavak@gmail.com>
2022-06-06 19:40:48 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char character;
+for (character = 'z' ; character >= 'a' ; character--)
+{
+putchar(character);
+}
+putchar('\n');
+return (0);
+}
