bd9a0d57b2f742b1335f430b35d519c12c5d0e4b
collinsnavak <collinsnavak@gmail.com>
2022-06-08 07:13:29 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - Prints th alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+char alpha;
+for (alpha = 'a'; <= 'z'; alpha++)
+{
+_putchar (alpha);
+}
+_putchar('\n');
+}
